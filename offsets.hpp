1f8605407a7a5318851f4de463459996313e155a
Henry <151407721+Henrycsharp@users.noreply.github.com>
2025-07-23 19:06:45 +0200

    -- Ignore

---
+++ offsets.hpp
@@ -0,0 +1,96 @@
+#pragma once
+#include <cstddef>
+
+namespace offsets {
+	//offets.hpp
+	constexpr std::ptrdiff_t dwEntityList = 0x1A05670;
+	constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x18590D0;
+	constexpr std::ptrdiff_t dwViewMatrix = 0x1A6E3F0;
+	constexpr std::ptrdiff_t dwGameRules = 0x1A69CB8;
+	constexpr std::ptrdiff_t dwViewAngles = 0x1A78650;
+	constexpr std::ptrdiff_t dwPlantedC4 = 0x1A07700;
+	constexpr std::ptrdiff_t dwNetworkGameClient = 0x53FCE0;
+	constexpr std::ptrdiff_t dwNetworkGameClient_signOnState = 0x228;
+	constexpr std::ptrdiff_t dwGlobalVars = 0x184CEB0;
+	constexpr std::ptrdiff_t dwGameTypes = 0x1A52E0;
+	constexpr std::ptrdiff_t dwGameTypes_mapName = 0x120;
+	//client.dll.hpp
+	constexpr std::ptrdiff_t m_hPlayerPawn = 0x824;
+	constexpr std::ptrdiff_t m_iHealthBarRenderMaskIndex = 0x1410;
+	constexpr std::ptrdiff_t m_iTeamNum = 0x3E3;
+	constexpr std::ptrdiff_t m_vOldOrigin = 0x1324;
+	constexpr std::ptrdiff_t m_iHealth = 0x344;
+	constexpr std::ptrdiff_t m_sSanitizedPlayerName = 0x778;
+	constexpr std::ptrdiff_t m_pGameSceneNode = 0x328;
+	constexpr std::ptrdiff_t m_modelState = 0x170;
+	constexpr std::ptrdiff_t m_fFlags = 0x3EC;
+	constexpr std::ptrdiff_t m_entitySpottedState = 0x23D0;
+	constexpr std::ptrdiff_t m_bSpotted = 0x8;;
+	constexpr std::ptrdiff_t m_flFlashBangTime = 0x13F8;
+	constexpr std::ptrdiff_t m_pCameraServices = 0x11E0;
+	constexpr std::ptrdiff_t m_iFOV = 0x210;
+	constexpr std::ptrdiff_t m_bIsScoped = 0x23E8;
+	constexpr std::ptrdiff_t m_iIDEntIndex = 0x1458;
+	constexpr std::ptrdiff_t m_szName = 0xD20;
+	constexpr std::ptrdiff_t m_pWeaponServices = 0x11A8;
+	constexpr std::ptrdiff_t m_iClip1 = 0x1678;
+	constexpr std::ptrdiff_t m_hActiveWeapon = 0x58;
+	constexpr std::ptrdiff_t m_pReserveAmmo = 0x1680;
+	constexpr std::ptrdiff_t m_bInReload = 0x17F8;
+	constexpr std::ptrdiff_t m_bHasDefuser = 0x40;
+	constexpr std::ptrdiff_t m_bHasHelmet = 0x41;
+	constexpr std::ptrdiff_t m_bHasHeavyArmor = 0x42;
+	constexpr std::ptrdiff_t m_hOwnerEntity = 0x440;
+	constexpr std::ptrdiff_t m_hMyWeapons = 0x40;
+	constexpr std::ptrdiff_t m_pItemServices = 0x11B0;
+	constexpr std::ptrdiff_t m_bDormant = 0xEF;
+	constexpr std::ptrdiff_t m_hPawn = 0x62C;
+	constexpr std::ptrdiff_t m_vecAbsOrigin = 0xD0;
+	constexpr std::ptrdiff_t m_vecVelocity = 0x400;
+	constexpr std::ptrdiff_t m_angEyeAngles = 0x1438;
+	constexpr std::ptrdiff_t m_iShotsFired = 0x23FC;
+	constexpr std::ptrdiff_t m_aimPunchAngle = 0x1584;
+	constexpr std::ptrdiff_t m_flSpread = 0xD54;
+	constexpr std::ptrdiff_t m_nBombSite = 0xF94;
+	constexpr std::ptrdiff_t m_flVelocityModifier = 0x2404;
+	constexpr std::ptrdiff_t m_pMovementServices = 0x11E8;
+	constexpr std::ptrdiff_t m_nButtons = 0x48;
+	constexpr std::ptrdiff_t m_pClippingWeapon = 0x13A0;
+	constexpr std::ptrdiff_t m_AttributeManager = 0x1148;
+	constexpr std::ptrdiff_t m_Item = 0x50;
+	constexpr std::ptrdiff_t m_bBombPlanted = 0x9A5;
+	constexpr std::ptrdiff_t m_bBombDropped = 0x9A4;
+	constexpr std::ptrdiff_t m_bBeingDefused = 0xFCC;
+	constexpr std::ptrdiff_t m_bBombTicking = 0xF90;
+	constexpr std::ptrdiff_t m_entitySpottedStateBomb = 0x1B58;
+	constexpr std::ptrdiff_t m_entitySpottedStateBombPlanted = 0xFA0;
+	constexpr std::ptrdiff_t m_vecOrigin = 0x568;
+	constexpr std::ptrdiff_t m_vecViewOffset = 0xCB0;
+	constexpr std::ptrdiff_t m_vSmokeColor = 0x121C;
+	constexpr std::ptrdiff_t m_bDidSmokeEffect = 0x1214;
+	constexpr std::ptrdiff_t m_totalRoundsPlayed = 0x84;
+	constexpr std::ptrdiff_t m_hObserverTarget = 0x44;
+	constexpr std::ptrdiff_t m_bGlowing = 0x51;
+	constexpr std::ptrdiff_t m_glowColorOverride = 0x40;
+	constexpr std::ptrdiff_t m_Glow = 0xC00;
+	constexpr std::ptrdiff_t m_pObserverServices = 0x11C0;
+	constexpr std::ptrdiff_t m_iItemDefinitionIndex = 0x1BA;
+	constexpr std::ptrdiff_t m_iObserverMode = 0x40;
+	//buttons.hpp
+	constexpr std::ptrdiff_t attack = 0x184E8F0;
+	constexpr std::ptrdiff_t attack2 = 0x184E980;
+	constexpr std::ptrdiff_t back = 0x184EBC0;
+	constexpr std::ptrdiff_t duck = 0x184EE90;
+	constexpr std::ptrdiff_t forward = 0x184EB30;
+	constexpr std::ptrdiff_t jump = 0x184EE00;
+	constexpr std::ptrdiff_t left = 0x184EC50;
+	constexpr std::ptrdiff_t lookatweapon = 0x1A75170;
+	constexpr std::ptrdiff_t reload = 0x184E860;
+	constexpr std::ptrdiff_t right = 0x184ECE0;
+	constexpr std::ptrdiff_t showscores = 0x1A75050;
+	constexpr std::ptrdiff_t sprint = 0x184E7D0;
+	constexpr std::ptrdiff_t turnleft = 0x184EA10;
+	constexpr std::ptrdiff_t turnright = 0x184EAA0;
+	constexpr std::ptrdiff_t use = 0x184ED70;
+	constexpr std::ptrdiff_t zoom = 0x1A750E0;
+}
